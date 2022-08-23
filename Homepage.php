d116c00f1e2d255524d17fe7247673f5b4f7aefc
HannaSchmuck <110851641+HannaSchmuck@users.noreply.github.com>
2022-08-23 08:09:53 +0200

    Anfang des scrollenden Bildes

--- Homepage.php
+++ Homepage.php
@@ -1,1 +1,74 @@
+<html>
 
+<head>
+
+  <link rel="stylesheet" href="https://github.com/HannaSchmuck/Autovermietung-x-HSBA/blob/6f3f13c5bf7db5ccf50dc8d757cf4938e0a13a70/stylesheet.css">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+</head>
+
+<body>
+
+<div class="navigation">
+  </div>
+<div>
+  <div class="parallax"></div>
+
+<div style="height:1000px;background-color:red;font-size:36px">
+Scroll Up and Down this page to see the parallax scrolling effect.
+This div is just here to enable scrolling.
+Tip: Try to remove the background-attachment property to remove the scrolling effect.
+</div>
+
+<div class="parallax"></div>
+ </div>
+
+
+
+<div style="width:100%; height:500px; background-color: #f8f9fa;">
+
+<div style="width:100%; height:250px">
+
+<div class="textboxleft" style="width:40%; height:220px;float:left;">
+	<h3> <a class="one" href="https://www.hsba.de/hsba/unsere-hochschule/departments/finance-accounting">Finance & Accounting</a></h3>
+	<p><a class="two" href="https://www.hsba.de/hsba/unsere-hochschule/departments/finance-accounting">Das Department koordiniert Aufgaben in Finance, Accounting, Corporate Governance, Wirtschaftsprüfung und Betriebswirtschaftliche Steuerlehre.</a></p>
+</div>
+
+<div class="textboxright" style="width:40%; height:220px;float:right;">
+	<h3><a class="one" href="https://www.hsba.de/hsba/unsere-hochschule/departments/finance-accounting">Marketing Transformation</a></h3>
+	<p><a class="two" href="https://www.hsba.de/hsba/unsere-hochschule/departments/finance-accounting">Das Department koordiniert Aufgaben in Finance, Accounting, Corporate Governance, Wirtschaftsprüfung und Betriebswirtschaftliche Steuerlehre.</a></p>
+</div>
+
+</div>
+
+<div style="width:100%; height: 250px;">
+
+<div class="textboxleft" style="width:40%; height:220px;float:left;">
+<h3><a class="one" href="https://www.hsba.de/hsba/unsere-hochschule/departments/finance-accounting">Strategy & Leadership</a></h3>
+<p><a class="two" href="https://www.hsba.de/hsba/unsere-hochschule/departments/finance-accounting">Das Department koordiniert Aufgaben in Finance, Accounting, Corporate Governance, Wirtschaftsprüfung und Betriebswirtschaftliche Steuerlehre.</a></p>
+</div>
+
+<div class="textboxright" style="width:40%; height:220px;float:right;">
+
+<h3><a class="one" href="https://www.hsba.de/hsba/unsere-hochschule/departments/finance-accounting">Technology & Markets</a></h3>
+<p><a class="two" href="https://www.hsba.de/hsba/unsere-hochschule/departments/finance-accounting">Das Department koordiniert Aufgaben in Finance, Accounting, Corporate Governance, Wirtschaftsprüfung und Betriebswirtschaftliche Steuerlehre.</a></p>
+
+</div>
+
+</div>
+<div style="width:100%; height:300px; background-color:grey;">
+	<div style="width:25%; height:300px;float:left;color:white">
+	<h3>BILDUNG</h3>
+	<a href="https://www.hsba.de/studium/bachelor/uebersicht">Bachelor-Studiengänge (Übersicht)</a>
+
+</div>
+
+	<div style="width:25%; height:300px;float:left;color:white">
+	<h3>POPULAR PAGES</h3>
+	<a href="https://www.hsba.de/studium/bachelor/duales-studium">Was ist ein duales Studium?</a>
+
+</div>
+	<div style="width:25%; height:300px;float:left;color:white">
+	<h3>KONTAKT</h3>
+	<a href="https://www.hsba.de/kontakt">Ansprechpartner</a>
+</div>
